abe2f3a14e892b3965d9ff455f124637e2b1e733
Fahad-smto <simanto.smto.prog@gmail.com>
2026-02-23 03:33:07 +0600

    sendResponse added

--- src/modules/Auth/auth.controller.ts
+++ src/modules/Auth/auth.controller.ts
@@ -1,29 +1,45 @@
 import { Request, Response } from "express";
 import { AuthService } from "./auth.service";
+import sendResponse from "../../utils/sendResponse";
 
 
 const createUser = async (req: Request, res: Response) => {
     try {
         const result = await AuthService.createUserIntoDB(req.body)
-        res.status(201).json({ message: "User created successfully", data: result })
+       sendResponse(res,{
+            statusCode:201,
+            success:true,
+            message:'user created',
+            data:result,
+        })
+     
     } catch (error) {
-        console.error("REGISTER ERROR 👉", error);
-        res.status(500).json({
-            message: "Internal Server Error",
-            error: error instanceof Error ? error.message : error
-        });
+       sendResponse(res,{
+            statusCode:500,
+            success:false,
+            message:'something went wrong',
+            data:error,
+        })
     }
 }
 const loginUser = async (req: Request, res: Response) => {
     try {
         const result = await AuthService.loginUserIntoDB(req.body)
-        res.status(200).json({ message: "User logged in successfully", data: result })
+
+        sendResponse(res,{
+            statusCode:201,
+            success:true,
+            message:'login successfully',
+            data:result,
+        })
+     
     } catch (error) {
-        console.error("LOGIN ERROR 👉", error);
-        res.status(500).json({
-            message: "Internal Server Error",
-            error: error instanceof Error ? error.message : error
-        });
+       sendResponse(res,{
+            statusCode:500,
+            success:false,
+            message:'something went wrong',
+            data:error,
+        })
     }
 }
 
