daf1a69da063641c21607947059b05048bcfcb42
Fahad-smto <simanto.smto.prog@gmail.com>
2026-02-23 02:40:37 +0600

    bcrypt added

--- src/modules/Auth/auth.service.ts
+++ src/modules/Auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { prisma } from "../../lib/prisma";
+import bcrypt from "bcrypt";
 
-const createUserIntoDB = async (payload: any) => {
-     const result = await prisma.user.create({
-        data: payload
-     })
+const createUserIntoDB = async (payload: any) =>{
+   const hashPassword = await bcrypt.hash(payload.password,10);
+
+        const result = await prisma.user.create({
+        data: {...payload,password:hashPassword}
+     });
+     const {password,...newresult}=result;
         return result
 }
 
