da7fe42127dda5bc0769336f8b576c0dda1f402f
Fahad-smto <simanto.smto.prog@gmail.com>
2026-07-17 23:12:01 +0600

    added code

--- src/modules/Admin/admin.route.ts
+++ src/modules/Admin/admin.route.ts
@@ -1,4 +1,8 @@
+import express from 'express';
+import { AdminController } from './admin.controller';
+import auth, { userRole } from '../../middlewares/auth';
 
+const router = express.Router();
 
 // 🔒   route ADMIN token  
 router.use(auth(userRole.ADMIN));
