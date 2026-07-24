e2addf538ebc5582366054b3200e82fe585c2a49
Fahad-smto <simanto.smto.prog@gmail.com>
2026-07-24 23:41:37 +0600

    code added

--- src/modules/Admin/admin.route.ts
+++ src/modules/Admin/admin.route.ts
@@ -11,6 +11,10 @@ router.use(auth(userRole.ADMIN));
 router.get('/users', AdminController.getAllUsers);
 router.patch('/users/:id', AdminController.updateUserStatus);
 
+// Categories
+router.get('/categories', AdminController.getAllCategories);
+router.post('/categories', AdminController.createCategory);
+router.delete('/categories/:id', AdminController.deleteCategory);
 
 // Bookings
 router.get('/bookings', AdminController.getAllBookings);
