4b50e689ab3b4caf4046417e4354d3f973a98d7a
Fahad-smto <simanto.smto.prog@gmail.com>
2026-07-25 23:54:28 +0600

    code added

--- src/modules/Review/review.route.ts
+++ src/modules/Review/review.route.ts
@@ -3,3 +3,11 @@ import { ReviewController } from './review.controller';
 import auth, { userRole } from '../../middlewares/auth';
 
 const router = express.Router();
+
+//   Student only — review  
+router.post('/', auth(userRole.STUDENT), ReviewController.createReview);
+
+//   Public —  tutor  reviews  
+router.get('/tutor/:tutorProfileId', ReviewController.getTutorReviews);
+
+export const ReviewRoutes = router;
