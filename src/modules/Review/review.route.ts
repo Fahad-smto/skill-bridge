a40df69d1f8240d1dd960504fb07b111bc232975
Fahad-smto <simanto.smto.prog@gmail.com>
2026-08-01 23:20:33 +0600

    banner and navbar added

--- src/modules/Review/review.route.ts
+++ src/modules/Review/review.route.ts
@@ -4,6 +4,9 @@ import auth, { userRole } from '../../middlewares/auth';
 
 const router = express.Router();
 
+//   Student only — review  
+router.post('/', auth(userRole.STUDENT), ReviewController.createReview);
+
 //   Public —  tutor  reviews  
 router.get('/tutor/:tutorProfileId', ReviewController.getTutorReviews);
 
